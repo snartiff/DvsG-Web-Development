ff8d48e073f15e6f2ff6782e620f68c70a402aa6
Sean Nartiff <sean.nartiff@gmail.com>
2018-03-04 12:57:05 -0500

    shortid

--- src/Blog.js
+++ src/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Parser from 'html-react-parser';
 import {Link} from 'react-router-dom';
-import shortid from 'shortid32';
+import shortid from 'shortid';
 import axios from 'axios';
 import render from 'react-dom';
 
